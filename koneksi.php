83f4cfe87f2822b1f15a4e56807c9c0b474d26cd
nirwannrs <nirwan.nrs@gmail.com>
2016-10-05 12:29:14 +0700

    deleted unused

--- koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = oci_connect('dbbarang', '1234', '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521)) (CONNECT_DATA=(SERVER=DEDICATED) (SERVICE_NAME = XE)))');
-if (!$conn){
-$e = oci_error();
-print htmlentities($e['message']);
-exit;
-}
-?>
